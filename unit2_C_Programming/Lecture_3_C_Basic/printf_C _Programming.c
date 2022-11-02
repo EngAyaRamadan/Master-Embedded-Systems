f23d8e77dc999102bacaafb9302faf9251ccb309
Aya-Ramadan <ayaramadan@gmail.com>
2022-11-02 06:29:23 +0200

    Frist step

---
+++ unit2_C_Programming/Lecture_3_C_Basic/printf_C _Programming.c
@@ -0,0 +1,16 @@
+/*
+ Name        : printf_C.c
+ Author      : Aya Ramadan
+ Version     : 1
+ Description : this code to print the C Programming
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("C Programming");
+
+	return 0;
+
+}
